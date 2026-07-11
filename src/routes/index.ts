9f7e4cd8d4077ee08d72b5f0403de91b59279ff0
nishat jahan <nishatjahanposhpa@gmail.com>
2026-07-11 14:30:31 +0600

    feat: added category modules

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import authRoutes from "../modules/auth/auth.routes.js";
+import categoryRoutes from "../modules/category/category.routes.js";
+
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/categories",categoryRoutes)
 
 export default router;
