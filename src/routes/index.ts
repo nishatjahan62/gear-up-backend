54f02b56107a3c40c1804fd652ce72cbacc7a7df
nishat jahan <nishatjahanposhpa@gmail.com>
2026-07-11 16:44:26 +0600

    feat: add review module with return-verification before posting

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,7 +5,7 @@ import categoryRoutes from "../modules/category/category.routes.js";
 import gearRoutes from "../modules/gear/gear.routes.js";
 import rentalRoutes from "../modules/rental/rental.routes.js";
 import paymentRoutes from "../modules/payment/payment.routes.js";
-// import reviewRoutes from "../modules/review/review.routes.js";
+import reviewRoutes from "../modules/review/review.routes.js";
 // import adminRoutes from "../modules/admin/admin.routes.js";
 
 const router = Router();
@@ -15,6 +15,6 @@ router.use("/categories",categoryRoutes)
 router.use("/gear",gearRoutes)
 router.use("/rentals",rentalRoutes)
 router.use("/payments",paymentRoutes)
-// router.use("/reviews",)
+router.use("/reviews",reviewRoutes)
 // router.use("/admin",)
 export default router;
